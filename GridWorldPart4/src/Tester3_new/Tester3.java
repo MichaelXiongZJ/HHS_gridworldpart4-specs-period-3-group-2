5d8eef71e832bc55f7e5e8294463da4fe5de04b0
Zejun Xiong <69989842+MichaelThePanda@users.noreply.github.com>
2021-02-08 15:04:16 -0800

    new tester3

--- GridWorldPart4/src/Tester3_new/Tester3.java
+++ GridWorldPart4/src/Tester3_new/Tester3.java
@@ -1,25 +1,25 @@
-package Tester5;
-
-
-
+package Tester3_new;
 
 import info.gridworld.actor.ActorWorld;
 import info.gridworld.actor.Bug;
 import info.gridworld.critters.HunterCritter;
 import info.gridworld.grid.Location;
 
-
-//A bug right in front of the hunter, another 3 bugs in the grid that is outside of its detection range.
-//The hunter should eats the bug in front and terrifies the other bugs
-public class Tester5 {
+public class Tester3 {
 	 public static void main(String[] args) {
 	        ActorWorld world = new ActorWorld();
 	        world.add(new Location(5, 5), new HunterCritter());
 	        world.add(new Location(4, 5), new Bug());
 	        
-	        world.add(new Location(9, 9), new Bug());
-	        world.add(new Location(1, 1), new Bug());
-	        world.add(new Location(5, 8), new Bug());
+	        for (int a=2; a<=8; a++) {
+	        	 world.add(new Location(1, a), new Bug());
+	        	 world.add(new Location(7, a), new Bug());
+	        }
+
+	        for (int a=2; a<=7; a++) {
+	        	 world.add(new Location(a, 2), new Bug());
+	        	 world.add(new Location(a, 8), new Bug());
+	        }
 	        
 	        world.show();
 	 }
